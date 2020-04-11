c810b03632527efb5148eacf5d8e9b430143368d
BenKatanHighLander <ben@highlander.io>
2020-04-11 18:40:12 +0300

    Updates

---
+++ precache-manifest.90eacea070a7bb85f40447c52987fce3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7f9a5a7b6605222087444dba828a7d4",
+    "url": "/speedGame/index.html"
+  },
+  {
+    "revision": "fffb5e915c5a71840d1d",
+    "url": "/speedGame/static/css/2.a6b37085.chunk.css"
+  },
+  {
+    "revision": "c5f6e52a7eafb5c4a427",
+    "url": "/speedGame/static/css/main.53d8e3e8.chunk.css"
+  },
+  {
+    "revision": "fffb5e915c5a71840d1d",
+    "url": "/speedGame/static/js/2.149033e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/speedGame/static/js/2.149033e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5f6e52a7eafb5c4a427",
+    "url": "/speedGame/static/js/main.9f79d55e.chunk.js"
+  },
+  {
+    "revision": "92c0bfd264595d057c95",
+    "url": "/speedGame/static/js/runtime-main.e0615952.js"
+  }
+]);
